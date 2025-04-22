647971f2052d8a9d75f121c9af85d02046337641
Jason Zimdars <jz@37signals.com>
2025-04-22 14:46:46 -0500

    Remove check for _Added by..._ line

--- test/models/event_summary_test.rb
+++ test/models/event_summary_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class EventSummaryTest < ActiveSupport::TestCase
   test "body" do
-    assert_equal "Added by David. Assigned to JZ.", event_summaries(:logo_initial_activity).body
+    assert_equal "Assigned to JZ.", event_summaries(:logo_initial_activity).body
     assert_equal "Assigned to Kevin.", event_summaries(:logo_second_activity).body
     assert_equal "Added by Kevin.", event_summaries(:text_initial_activity).body
   end
